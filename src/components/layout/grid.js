ef158606b351938ef2bdf5c9b0a19a9e7f51c503
ThrivingKings <danielraftery00@gmail.com>
2017-07-11 22:26:17 -0400

    adds minWidth to Column component

--- src/components/layout/grid.js
+++ src/components/layout/grid.js
@@ -10,28 +10,31 @@ const Columns = ({ children, ...props }) => (
 const Column = ({
   children,
   maxWidth,
+  minWidth,
   flexGrow = 'initial',
   flexShrink = 'initial',
   ...props
 }) => (
-  <div {...styles.column(maxWidth, flexGrow, flexShrink)} {...props}>
+  <div {...styles.column(maxWidth, minWidth, flexGrow, flexShrink)} {...props}>
     { children }
   </div>
 )
 
 Column.propTypes = {
   maxWidth: PropTypes.string,
+  minWidth: PropTypes.string,
   flexGrow: PropTypes.string,
   flexShrink: PropTypes.string
 }
 
 const styles = {
-  column: (maxWidth, flexGrow, flexShrink) => css({
+  column: (maxWidth, minWidth, flexGrow, flexShrink) => css({
     flexGrow,
     flexShrink,
     height: '100%',
     width: '100%',
-    maxWidth
+    maxWidth,
+    minWidth
   }),
   columns: css({
     display: 'flex',
